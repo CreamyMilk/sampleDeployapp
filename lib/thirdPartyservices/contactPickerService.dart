8b9cc0d84ce0eefd6bdc9f5bb1f037cddfd626cf
CreamyMilk <creamym254@gmail.com>
2020-10-01 10:45:52 +0300

    Removed contact service👏

--- lib/thirdPartyservices/contactPickerService.dart
+++
@@ -1,8 +0,0 @@
-import 'package:fluttercontactpicker/fluttercontactpicker.dart';
-
-class ContactPickerService {
-  Future<PhoneContact> getContact() async {
-    final PhoneContact contact = await FlutterContactPicker.pickPhoneContact();
-    return contact;
-  }
-}
