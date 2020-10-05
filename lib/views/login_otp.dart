2331a91f32ee053d0878d0bf6f0d15abf3e38ec0
CreamyMilk <creamym254@gmail.com>
2020-10-05 17:45:44 +0300

    Stanch clone

--- lib/views/login_otp.dart
+++ lib/views/login_otp.dart
@@ -2,10 +2,10 @@ import 'dart:convert';
 
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:fluttercontactpicker/fluttercontactpicker.dart';
+//import 'package:fluttercontactpicker/fluttercontactpicker.dart';
 import 'package:sampledeployapp/model/otpresponse.dart';
-import 'package:sampledeployapp/widget/otp_receiver.dart';
-import 'package:sms_autofill/sms_autofill.dart';
+//import 'package:sampledeployapp/widget/otp_receiver.dart';
+//import 'package:sms_autofill/sms_autofill.dart';
 import 'package:http/http.dart' as http;
 
 class LoginOTP extends StatefulWidget {
@@ -15,96 +15,250 @@ class LoginOTP extends StatefulWidget {
 }
 
 class _LoginOTPState extends State<LoginOTP> {
-  final TextEditingController _testcontroller = TextEditingController();
+  //final TextEditingController _testcontroller = TextEditingController();
   String mobile = "";
   bool term = false;
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.lightGreen,
-      body: Center(
-        child: AnimatedContainer(
-          duration: Duration(seconds: 3),
-          child: SingleChildScrollView(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+          leading: IconButton(
+              icon: Icon(
+                Icons.notifications,
+                color: Colors.white,
+              ),
+              onPressed: () {}),
+          title: Center(
+            child: Row(children: [
+              Spacer(),
+              Text("ICRIB\nAgency",
+                  style: TextStyle(fontWeight: FontWeight.w500)),
+              Icon(
+                Icons.tag_faces,
+                color: Colors.white,
+                size: 40,
+              ),
+              Spacer()
+            ]),
+          ),
+          actions: [
+            IconButton(
+                icon: Icon(
+                  Icons.settings,
+                  color: Colors.white,
+                ),
+                onPressed: () {})
+          ],
+        ),
+        body: Column(
+          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+          children: [
+            SizedBox(height: 210),
+            Row(
               children: [
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.center,
-                  children: [
-                    Checkbox(
-                      onChanged: (bool value) {
-                        setState(() {
-                          term = !term;
-                          value = !value;
-                        });
-                      },
-                      value: term,
-                    ),
-                    Text("I accept the Terms & Conditions"),
-                  ],
-                ),
-                SizedBox(
-                  height: 50,
-                ),
-                //Text("Enter Mobile number below"),
-                Container(
-                  padding: EdgeInsets.symmetric(
-                    horizontal: 50,
-                    vertical: 10,
-                  ),
-                  child: TextField(
-                    onChanged: (value) {
-                      print(value);
-                      setState(() {
-                        mobile = value;
-                      });
-                    },
-                    controller: _testcontroller,
-                    decoration: InputDecoration(
-                      suffixIcon: IconButton(
-                        icon: Icon(Icons.perm_contact_calendar),
-                        onPressed: () async {
-                          final PhoneContact contact =
-                              await FlutterContactPicker.pickPhoneContact();
-                          print(contact);
-                          setState(() {
-                            _testcontroller.text = contact.phoneNumber.number;
-                            mobile = contact.phoneNumber.number;
-                          });
-                        },
-                      ),
-                      hintText: 'PhoneNumber',
-                      errorText: validatePassword(_testcontroller.text),
-                      prefixText: "",
-                    ),
-                    keyboardType: TextInputType.numberWithOptions(),
-                  ),
-                ),
-                MaterialButton(
-                  color: term ? Colors.blue : Colors.grey,
+                Padding(
+                  padding: const EdgeInsets.only(left: 20.0),
                   child: Text(
-                    "Click me",
-                    style: TextStyle(color: Colors.white),
+                    "Welcome To \nIcrib Residence",
+                    style: TextStyle(
+                      color: Colors.white,
+                      fontSize: 25,
+                    ),
                   ),
-                  onPressed: () async {
-                    //send post request here
-                    if (term) {
-                      final appsignature = await SmsAutoFill().getAppSignature;
-                      getOTP(_testcontroller.text, appsignature);
-                      Navigator.push(context,
-                          CupertinoPageRoute(builder: (c) => OtpReceiver()));
-                    } else {
-                      print("snack");
-                    }
-                  },
                 ),
+                Spacer(flex: 1),
+                MaterialButton(
+                    shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.only(
+                        topLeft: Radius.circular(8.0),
+                        bottomLeft: Radius.circular(8.0),
+                      ),
+                    ),
+                    padding: EdgeInsets.all(20.0),
+                    onPressed: () {},
+                    color: Colors.green,
+                    child: Row(
+                      children: [
+                        Text(
+                          "LOGIN",
+                          style: TextStyle(
+                              color: Colors.white,
+                              fontWeight: FontWeight.w400,
+                              fontSize: 13),
+                        ),
+                        Icon(
+                          Icons.lock,
+                          size: 10,
+                          color: Colors.white,
+                        )
+                      ],
+                    ))
               ],
             ),
-          ),
-        ),
-      ),
-    );
+            Spacer(flex: 2),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  SizedBox(width: 10),
+                  Expanded(
+                    flex: 2,
+                    child: Container(
+                        height: 200,
+                        color: Colors.white,
+                        child: Text("See House Listings")),
+                  ),
+                  SizedBox(width: 5),
+                  Expanded(
+                    flex: 1,
+                    child: Stack(
+                      children: [
+                        Container(
+                          alignment: Alignment.bottomCenter,
+                          height: 200,
+                          color: Colors.black,
+                          child: LayoutBuilder(builder: (context, constraints) {
+                            return Container(
+                              height: 100,
+                              width: constraints.maxWidth,
+                              color: Colors.grey,
+                              child: Column(
+                                mainAxisAlignment:
+                                    MainAxisAlignment.spaceAround,
+                                children: [
+                                  Icon(
+                                    Icons.help_outline,
+                                    size: constraints.maxWidth / 2.5,
+                                    color: Colors.white,
+                                  ),
+                                  Text("Help")
+                                ],
+                              ),
+                            );
+                          }),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 5),
+                  Expanded(
+                    flex: 1,
+                    child: Stack(
+                      children: [
+                        Container(
+                          alignment: Alignment.bottomCenter,
+                          height: 200,
+                          color: Colors.black,
+                          child: LayoutBuilder(builder: (context, constraints) {
+                            return ClipRRect(
+                              borderRadius: BorderRadius.circular(4.0),
+                              child: Container(
+                                decoration: BoxDecoration(
+                                  color: Colors.grey,
+                                  border: Border(
+                                    bottom: BorderSide(
+                                        width: 3.0, color: Colors.transparent),
+                                  ),
+                                ),
+                                height: 100,
+                                width: constraints.maxWidth,
+                                child: Column(
+                                  mainAxisAlignment:
+                                      MainAxisAlignment.spaceAround,
+                                  children: [
+                                    Icon(
+                                      Icons.help_outline,
+                                      size: constraints.maxWidth / 2.5,
+                                      color: Colors.white,
+                                    ),
+                                    Text("Help")
+                                  ],
+                                ),
+                              ),
+                            );
+                          }),
+                        ),
+                      ],
+                    ),
+                  ),
+                  SizedBox(width: 5),
+                ],
+              ),
+            ),
+            // Row(
+            //   mainAxisAlignment: MainAxisAlignment.center,
+            //   children: [
+            //     Checkbox(
+            //       onChanged: (bool value) {
+            //         setState(() {
+            //           term = !term;
+            //           value = !value;
+            //         });
+            //       },
+            //       value: term,
+            //     ),
+            //     Text("I accept the Terms & Conditions"),
+            //   ],
+            // ),
+            // SizedBox(
+            //   height: 50,
+            // ),
+            //Text("Enter Mobile number below"),
+            // Container(
+            //   padding: EdgeInsets.symmetric(
+            //     horizontal: 50,
+            //     vertical: 10,
+            //   ),
+            //   child: TextField(
+            //     onChanged: (value) {
+            //       print(value);
+            //       setState(() {
+            //         mobile = value;
+            //       });
+            //     },
+            //     controller: _testcontroller,
+            //     decoration: InputDecoration(
+            //       suffixIcon: IconButton(
+            //         icon: Icon(Icons.perm_contact_calendar),
+            //         onPressed: () async {
+            //           final PhoneContact contact =
+            //               await FlutterContactPicker.pickPhoneContact();
+            //           print(contact);
+            //           setState(() {
+            //             _testcontroller.text = contact.phoneNumber.number;
+            //             mobile = contact.phoneNumber.number;
+            //           });
+            //         },
+            //       ),
+            //       hintText: 'PhoneNumber',
+            //       errorText: validatePassword(_testcontroller.text),
+            //       prefixText: "",
+            //     ),
+            //     keyboardType: TextInputType.numberWithOptions(),
+            //   ),
+            // ),
+            // MaterialButton(
+            //   color: term ? Colors.blue : Colors.grey,
+            //   child: Text(
+            //     "Click me",
+            //     style: TextStyle(color: Colors.white),
+            //   ),
+            //   onPressed: () async {
+            //     //send post request here
+            //     if (term) {
+            //       final appsignature = await SmsAutoFill().getAppSignature;
+            //       getOTP(_testcontroller.text, appsignature);
+            //       Navigator.push(context,
+            //           CupertinoPageRoute(builder: (c) => OtpReceiver()));
+            //     } else {
+            //       print("snack");
+            //     }
+            //   },
+            // ),
+          ],
+        ));
   }
 }
 
